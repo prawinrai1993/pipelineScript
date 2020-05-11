5681678d2ed93c0593756bfe1b0df09b16a5add7
prawinrai <prawinrai@hotmail.com>
2020-05-12 02:43:57 +0530

    updated

--- create_libraries.sh
+++ create_libraries.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-gcc -c ../mathlib.c -o ../mathlib.o
-gcc ../mathlib.o -shared -o ../libmath.so
+gcc -c mathlib.c -o mathlib.o
+gcc mathlib.o -shared -o libmath.so
 
 echo "libraries generated successfully!!!"
