2ad6bd2edd1927bc3be685ad9dcb59c1a8f31a39
prawinrai <prawinrai@hotmail.com>
2020-05-12 02:10:39 +0530

    scripts added

---
+++ create_libraries.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+gcc -c mathlib.c -o mathlib.o
+gcc mathlib.o -shared -o libmath.so
+
+echo "libraries generated successfully!!!"
---
+++ generate_coverage.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo gcc execute_MathLib.c libmath.so -o executable -fprofile-arcs -ftest-coverage
+
+LD_LIBRARY_PATH=$(pwd)/
+export LD_LIBRARY_PATH
+./executable 
+
+lcov -c -d . -o coverage.info
+
+genhtml coverage.info -o coverage
+
+echo "coverage generated successfully!!!"
